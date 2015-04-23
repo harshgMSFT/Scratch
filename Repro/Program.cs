5019d72a8549ceb30decf5707b9d715dddc11442
Harsh Gupta <harshg@microsoft.com>
2015-04-23 10:43:14 -0700

    Adding First Project

---
+++ Repro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashSet = new HashSet<string>();
+            hashSet.Add("ada");
+            hashSet.Add("ada1");
+            hashSet.Add("ada2");
+            IReadOnlyCollection<string> hash = (IReadOnlyCollection<string>)hashSet;
+        }
+    }
+}
